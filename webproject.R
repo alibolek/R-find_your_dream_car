527eb476c9cb277a9b1071929cc475576ed54b1f
erayerturk <erturkeraymelih@gmail.com>
2017-05-14 21:05:33 +0300

    points

--- webproject.R
+++ webproject.R
@@ -1,29 +1,47 @@
 library(shiny)
 library(leaflet)
 library(ggmap)
+library(geosphere)
+
 
 server <- function(input, output) {
   
   output$map <- renderLeaflet({
     
     # Get latitude and longitude
-    if(input$target_zone=="Ex: Bamako"){
-      ZOOM=2
-      LAT=0
-      LONG=0
+    if(input$target_zone=="ex. Muğla"){
+      ZOOM=5
+      LAT=37.21537
+      LONG=28.36339
+      
+      leaflet() %>% 
+        setView(lng=LONG, lat=LAT, zoom=ZOOM ) %>%
+        addTiles() %>% addMarkers(lng=cars.all$lon, lat=cars.all$lat, popup=paste("slm",'<br><a href="http://www.google.com">Web Site</a>'))
+      
+      
     }else{
       target_pos=geocode(input$target_zone)
       LAT=target_pos$lat
       LONG=target_pos$lon
       ZOOM=5
       
+      
+      
+      cars.all <- cars.all[cars.all$lon <= (LONG+1) & cars.all$lon >= (LONG-1),]
+      cars.all <- cars.all[cars.all$lat <= (LAT+1) & cars.all$lat >= (LAT-1),]
+      
       leaflet() %>% 
         setView(lng=LONG, lat=LAT, zoom=ZOOM ) %>%
-        addTiles() %>% addMarkers(lng=32.85974, lat=39.93336, popup=paste("slm",'<br><a href="http://www.google.com">Web Site</a>'))
+        addTiles() %>% addMarkers(lng=cars.all$lon, lat=cars.all$lat, popup=paste(cars.all$shortname,'<br><a href="http://www.google.com">Web Site</a>'),clusterOptions = markerClusterOptions())
+      
+      
       
     }
     
     # Plot it!
+    #cars.all <- cars.all[cars.all[,7] == input$target_zone]
+    
+    
     
   })
 }
@@ -32,7 +50,7 @@ server <- function(input, output) {
 ui <- fluidPage(
   br(),
   leafletOutput("map", height="600px"),
-  absolutePanel(top=20, left=70, textInput("target_zone", "" , "Your City")),
+  absolutePanel(top=20, left=70, textInput("target_zone", "" , "ex. Muğla")),
   
   
   absolutePanel(top=80, left=70,selectInput(selected = 1,"shortname", "Cars",
